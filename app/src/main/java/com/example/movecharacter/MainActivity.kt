71487fde16022fbea4f0b1a1d90f6611dc470514
flex2020 <61104736+flex2020@users.noreply.github.com>
2022-04-07 20:03:32 +0900

    start

--- app/src/main/java/com/example/movecharacter/MainActivity.kt
+++ app/src/main/java/com/example/movecharacter/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.movecharacter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import com.example.movecharacter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    var moveX = 0f
+    var moveY = 0f
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.character.setOnTouchListener { v, event ->
+            when(event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    moveX = v.x - event.rawX
+                    moveY = v.y - event.rawY
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    v.animate()
+                            .x(event.rawX + moveX)
+                            .y(event.rawY + moveY)
+                            .setDuration(0)
+                            .start()
+                }
+            }
+            true
+        }
     }
+
+
+
+
+
 }
